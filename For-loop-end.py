d7d4412f6ad24b9d06b0928c744d83d9f7756f13
Maruthees <43824508+Maruthees@users.noreply.github.com>
2020-01-04 09:57:33 +0530

    Add files via upload
    For loop explained with end parameter for printing the arguments

---
+++ For-loop-end.py
@@ -0,0 +1,22 @@
+
+x="kohli"
+#x=[1,2,3,6,7]
+
+##for i in x:
+##    print(i)
+
+
+#for i in range (where to start , where to end , step size)
+    
+for i in range(2,10,2):
+    print (i,end=" between ")  #prints horizontally or prevents the default new line character by the value provided within quotes
+                               #or what the line needs to print when done completion of the argument
+    
+     
+
+
+for i in range(10):
+     if i%2==0:
+       print("Even",i)
+     else:
+       print("Odd",i)
