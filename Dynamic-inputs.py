9d883e61196cad93d63bc06f11f2f336845c04d7
Maruthees <43824508+Maruthees@users.noreply.github.com>
2020-01-11 00:12:06 +0530

    Add files via upload
    Python always takes input as strings since it only understands string , we have to intentionally mention it as other data types

---
+++ Dynamic-inputs.py
@@ -0,0 +1,12 @@
+
+#Input dynamically gets the value from user
+
+age=int(input("Enter your age"))
+age1_not_mentioned=input("Enter your age")
+print(type(age))
+print("Type of age1_not_mentioned",type(age1_not_mentioned))
+print(type(age1_not_mentioned))
+print(age)
+age1=10
+
+print(age+age1)
