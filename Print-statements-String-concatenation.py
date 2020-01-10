c944b443a8a9368bcb6bd097667d4456f92c6688
Maruthees <43824508+Maruthees@users.noreply.github.com>
2020-01-11 00:01:07 +0530

    Add files via upload

---
+++ Print-statements-String-concatenation.py
@@ -0,0 +1,29 @@
+#String concatenation
+#Type-casting 
+
+var="Dhoni"
+age=37
+#print("Hello" + var + "Welcome" +age) #Integer cannot be added to string
+print("Hello" + var + "Welcome") #Prints without space---> HelloDhoniWelcome
+print("hello" +var+"Welcome"+str(age)) #Typecasting
+
+#Instead of using + concatenation we can use commas ,
+
+print("hello",var,"Welcome",age) #Perfect colution to print string & variables
+
+#Another solution to this
+
+print("Hello %s welcome your age is %d"%(var,age)) #Perfect 2nd solution
+
+#Format string when dynamcially we get string or integers
+runs=34
+print("{} Run scored by Dhoni".format(runs))
+runs="thirty four"
+print("{} Run scored by Dhoni".format(runs))
+
+##    print("Hello" + var + "Welcome" +age)
+##TypeError: can only concatenate str (not "int") to str
+
+a='10'
+b='20'
+print(a+b) #1020
