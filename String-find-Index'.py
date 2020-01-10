3c5a68b2bb14e046ea2c7fcc1f776bb436031572
Maruthees <43824508+Maruthees@users.noreply.github.com>
2020-01-11 00:06:45 +0530

    Add files via upload
    String with functions count ( ) , find( ) , rfind ( ) , index ( )

---
+++ String-find-Index'.py
@@ -0,0 +1,13 @@
+
+#Find returns a value -1 and continues the next statement when ther is no value
+#Index stops the line as there is no match & stops the code
+#System defined are called as Methods
+
+var="India is my history"
+print(var.find('z'))
+print("over")
+
+
+var="India is my history"
+print(var.index('z'))
+print("over")
