5925de00e2a53b21614c98fb634f9526dd2472a0
Maruthees <43824508+Maruthees@users.noreply.github.com>
2020-01-10 23:43:36 +0530

    Add files via upload
    Defines ord () & chr()

---
+++ For-ASCII-conversion.py
@@ -0,0 +1,16 @@
+
+#ASCI value of a is 97
+#character value of 97 is a
+
+print(ord('a'))  # ord function returns ASCII values- Character to ASCII--always characeter
+print(chr(97))   # chr function returns character value of the given ASCII always integer
+print(chr(ord('a')) 
+
+for x in range(26):
+    print(chr(97))
+    
+###Print a to z
+
+ 
+for i in range (26):
+      print(chr(i+97))
