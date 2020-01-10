85a42e3605ae704a6c6835b8751d50a939342184
Maruthees <43824508+Maruthees@users.noreply.github.com>
2020-01-11 00:25:00 +0530

    Add files via upload

---
+++ List-basics.py
@@ -0,0 +1,24 @@
+
+#List LIFO , stack data structure ,keeps stack of books , book is placed a top & removed from top position
+var=["dhoni","kohli"]
+var.append('zahher')
+print (var)
+var.pop()
+print(var)
+
+
+##var.insert(1,'ashwin')
+##print(var)
+
+var[1]='ashwin'
+print(var)
+
+var.sort()
+print(var)
+
+
+var.pop(0)
+print(var)
+
+var1=var
+print(var1)
